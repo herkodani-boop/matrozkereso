7fd8e6600dd723cd4a66ce5cb91fecf0ea4c3f0b
unknown <you@example.com>
2026-09-26 10:18:28 +0200

    Csapattagok

--- app/layout.tsx
+++ app/layout.tsx
@@ -17,19 +17,19 @@ export const metadata: Metadata = {
   icons: {
     icon: [
       {
-        url: '/icon-light-32x32.png',
+        url: '/logo-mark.png',
         media: '(prefers-color-scheme: light)',
       },
       {
-        url: '/icon-dark-32x32.png',
+        url: '/logo-mark.png',
         media: '(prefers-color-scheme: dark)',
       },
       {
-        url: '/icon.svg',
+        url: '/logo-mark.svg',
         type: 'image/svg+xml',
       },
     ],
-    apple: '/apple-icon.png',
+    apple: '/logo-mark.png',
   },
 }
 
